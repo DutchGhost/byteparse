8f43f44bfea54398828a7f53e12cd4050cc5c5c1
DutchGhost <kasper199914@gmail.com>
2018-08-22 20:59:01 +0200

    add comments

--- byteparse.zig
+++ byteparse.zig
@@ -1,12 +1,15 @@
 const std = @import("std");
 
+/// Returns how many digits a base-10 number has.
+/// Gets compiled down to many if's.
 pub fn digits10(comptime N: type, n: N) usize {
-    comptime var digits = 0;
-    comptime var check = 1;
+    comptime var digits = 1;
+    comptime var check = 10;
 
     inline while (check <= @maxValue(N)) : ({check *= 10; digits += 1;}) {
-        if (n < check)
+        if (n < check) {
             return digits;
+        }
     }
 
     return digits;
@@ -17,6 +20,7 @@ const ParseError = error {
     InvalidCharacter,
 };
 
+/// Returns a slice containing all the multiple powers of 10 that fit in the integer type `N`.
 pub fn pow10array(comptime N: type) []N {
     comptime var multiple_of_ten: N = 1;
 
@@ -38,23 +42,10 @@ pub fn pow10array(comptime N: type) []N {
     return table[0..];
 }
 
+/// Converts a byte-slice into the integer type `N`.
+/// if the byte-slice contains a digit that is not valid, an error is returned.
+/// An empty slice returns 0.
 pub fn atoi(comptime N: type, buf: []const u8) ParseError!N {
-    //comptime var multiple_of_ten: N = 1;
-
-    //comptime var table_size = comptime digits10(N, @maxValue(N));
-    //comptime var counter = 0;
-
-    //inline while(counter + 1 < table_size): ({counter += 1;}) {
-    //    multiple_of_ten *= 10;
-    //}
-
-    // generate table
-    //comptime var table: [table_size] N = undefined;
-
-    //inline for(table) |*num| {
-    //    num.* = multiple_of_ten;
-    //    multiple_of_ten /= 10;
-    //}
 
     comptime var table = comptime pow10array(N);
 
@@ -68,27 +59,27 @@ pub fn atoi(comptime N: type, buf: []const u8) ParseError!N {
         if (r1 > 9) {
             return ParseError.InvalidCharacter;
         }
-        var d1: N = r1 * table[idx];
+        var d1 = r1 * table[idx];
 
         var r2 = bytes[1] -% 48;
         if (r2 > 9) {
             return ParseError.InvalidCharacter;
         }
-        var d2: N = r2 * table[idx + 1];
+        var d2 = r2 * table[idx + 1];
 
         var r3 = bytes[2] -% 48;
         if (r3 > 9) {
             return ParseError.InvalidCharacter;
         }
-        var d3: N = r3 * table[idx + 2];
+        var d3 = r3 * table[idx + 2];
 
         var r4 = bytes[3] -% 48;
         if (r4 > 9) {
             return ParseError.InvalidCharacter;
         }
-        var d4: N = r4 * table[idx + 3];
+        var d4 = r4 * table[idx + 3];
 
-        result = result +% d1 +% d2 +% d3 +% d4;
+        result = result +% (d1 + d2 + d3 + d4);
 
         len -= 4;
         idx += 4;
